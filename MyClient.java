68234e1f258d6575664521173c5183b1dcfae3eb
zarrafkhan <zarraf.khan@students.mq.edu.au>
2022-04-04 02:22:10 +1000

    Added largest server type search and count

--- MyClient.java
+++ MyClient.java
@@ -1,55 +1,88 @@
 import java.net.*;
 import java.util.ArrayList;
 import java.io.*;  
+
 class MyClient{  
 public static void main(String args[])throws Exception{  
 
 String mess = ""; //last message
-String data = ""; //server state info 
-String[] nRecs; 
+String[] minfo; //specfic information from last message
+
+ArrayList<String[]> data = new ArrayList<String[]>();  
+String ltype = "";
+int lnum = 0; //number of largest server types (limit)
+String jobID = ""; 
+String[] recs; // Includes n.o. Records and size 
 
 try{
 
+//default server connection
 Socket s=new Socket("localhost",50000);  
-//DataInputStream din=new DataInputStream(s.getInputStream());  
 DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
 BufferedReader bin = new BufferedReader(new InputStreamReader(s.getInputStream()));  
 
+//basic server exchanges
 dout.write(("HELO\n").getBytes());
-dout.flush();
 bin.readLine();
 dout.write(("AUTH zeek\n").getBytes());
-dout.flush();
-//step 5
+bin.readLine();
+
 dout.write(("REDY\n").getBytes());      
-mess = bin.readLine(); //recieves jobn first
+mess = bin.readLine(); //recieves jobn first           
+minfo = mess.split(" "); 
+jobID = minfo[2];
+//Sends GETS Capable Core/Memory/Disk
+dout.write(("GETS Capable " + minfo[4] + " " + minfo[5] + " " + minfo[6] + " \n").getBytes()); 
+recs = bin.readLine().split(" "); //DATA nRecs Size
+int nRecs = Integer.parseInt(recs[1]);
+dout.write(("OK\n").getBytes());   
 
-//IDENTIFY LARGEST
-dout.write(("GETS ALL\n").getBytes());
-nRecs = bin.readLine().substring(5).split(" ");
-dout.write(("OK\n").getBytes());
-data = bin.readLine(); //first split by /n, then  " " into string[][] or arraylist
-for (int i = 0;i < Integer.parseInt(nRecs[1]); i++){ //foreach record
-
+String[] d = bin.readLine().split("\n"); //Server state info
+for(int i =0; i< nRecs; i++){ //for each record store into list
+    data.add(d[i].split(" "));
 }
 
-while(!mess.equals("NONE\n")){
-    //step 7
-    //check for largest server type
-    //and 
-    if (mess.startsWith("JOBN")){                     
-        //SEND GETS
-        //RECIEVE DATA NRECS SIZE
-        //SEND OK
-        //For NRECS Arr
-        //
+//FIND LARGEST type based on core size
+ltype = data.get(0)[0];
+for (int i =1; i< nRecs; i++){
+    if (Integer.parseInt(data.get(i)[4]) > Integer.parseInt(data.get(i-1)[4])){
+        ltype = data.get(i)[0];
+    }
+}
+
+for (String[] a: data){ //acquire largest server count
+    if (a[0].equals(ltype)){
+        lnum++;
+    }
+}
+
+
+dout.write(("OK\n").getBytes());  
+bin.readLine(); 
+
+while( !(mess.equals("NONE\n")) ){
+
+    if (mess.startsWith("JOBN")){        
+        dout.write(("SCHD " + jobID + " " + ltype + "\n").getBytes());   //iterate through 0 to (lnum-1)
+
+        dout.write(("REDY\n").getBytes());      
+        mess = bin.readLine(); 
+        minfo = mess.split(" "); 
+        jobID = minfo[2];
+        dout.write(("GETS Capable " + minfo[4] + " " + minfo[5] + " " + minfo[6] + " \n").getBytes()); 
+        bin.readLine();
+        dout.write(("OK\n").getBytes());  
+        bin.readLine();
+        dout.write(("OK\n").getBytes());  
+        bin.readLine();  
     }
 }
 
 dout.write(("QUIT\n").getBytes());
 dout.flush();
-data = bin.readLine();
+bin.readLine();
 dout.close();  
+
 s.close(); 
  
 }catch(Exception e){System.out.println(e);}
@@ -59,23 +92,7 @@ s.close();
 
 
 /************************ 
-POSSIBLE LAYOUT
-
-DO-ONLY ONCE STUFF FIRST (AUTH, READLINE (RECIEVE JOBN), IDENTIFY LARGEST SERVER TYPE USING GETS ALL - CORES)
-(IF NONE -> QUIT)
-
-WHILE NONE LOOP{ ROUND ROBIN
-    IF (JOBN) SCHD
-    IF (ANYTHING ELSE) X...
-}
-
-
-
-
-
-
-
-
+NOTES
 
 
 */
